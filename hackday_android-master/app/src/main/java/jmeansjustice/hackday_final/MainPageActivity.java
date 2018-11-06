03fad9afc2b9478cfdad83738b4d7f7fca4f74f4
Madi Sakhabanov <skhbnv@gmail.com>
2018-11-06 19:01:39 +0600

    asdasd

---
+++ hackday_android-master/app/src/main/java/jmeansjustice/hackday_final/MainPageActivity.java
@@ -0,0 +1,14 @@
+package jmeansjustice.hackday_final;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainPageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+
+}
---
+++ hackday_android-master/app/src/main/java/jmeansjustice/hackday_final/Users.java
@@ -0,0 +1,41 @@
+package jmeansjustice.hackday_final;
+
+public class Users {
+    private String mName;
+    private String mSurname;
+    private String mGender;
+    private String mAge;
+    private String mPhone;
+
+    Users(){
+
+    }
+
+    public Users(String mName, String mSurname, String mGender, String mAge, String mPhone) {
+        this.mName = mName;
+        this.mSurname = mSurname;
+        this.mGender = mGender;
+        this.mAge = mAge;
+        this.mPhone = mPhone;
+    }
+
+    public String getmAge() {
+        return mAge;
+    }
+
+    public String getmGender() {
+        return mGender;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public String getmSurname() {
+        return mSurname;
+    }
+
+    public String getmPhone() {
+        return mPhone;
+    }
+}
